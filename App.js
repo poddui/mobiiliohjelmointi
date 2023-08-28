2a3b88f0dd1fa16906d33ecbcb186858672042f5
poddui <jimi8888.podduikin@gmail.com>
2023-08-28 10:53:49 +0300

    Teht 2.

--- App.js
+++ App.js
@@ -1,49 +1,69 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity, Modal} from 'react-native';
 
 export default function App() {
 
-  const [number1, onChangeNumber1] = useState('0');
-  const [number2, onChangeNumber2] = useState('0');
-  const [result, setResult] = useState(0);
+  const [number, setNumber] = useState('0');
+  const [randomNumber, setRandomNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  const [guess, setGuess] = useState(0);
+  const [guesText, setGuessText] = useState('');
+  const [isModalVisible, setModalVisible] = useState(false);
 
-  const addNumbers = () => {
-    const sum = parseInt(number1) + parseInt(number2);
-    setResult(sum)
+  const comparing = () => {
+    setGuess(guess + 1)
+    if (randomNumber == parseInt(number)){
+      toggleModal()
+    }
+    else if (randomNumber > parseInt(number)) {
+      setGuessText(`Your guess ${number} is too low`);
+    }
+    else{
+      setGuessText(`Your guess ${number} is too high`);
+    }
   };
 
-  const minNumbers = () => {
-    const diff = number1 - number2;
-    setResult(diff)
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const reset = () => {
+    toggleModal()
+    setRandomNumber(Math.floor(Math.random() * 100) + 1);
+    setGuess(0)
   };
 
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.inputContainer}>
-      <Text style={styles.baseText}>Result: {result}</Text>
+      <Text style={styles.baseText}>{guesText}</Text>
         <TextInput
           style={styles.input}
-          value={number1}
-          onChangeText={onChangeNumber1}
-          placeholder="Numero 1"
-          keyboardType="numeric"
-        />
-        <TextInput
-          style={styles.input}
-          value={number2}
-          onChangeText={onChangeNumber2}
-          placeholder="Numero 2"
+          value={number}
+          onChangeText={setNumber}
+          placeholder="Numero"
           keyboardType="numeric"
         />
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={addNumbers}>
-            <Text style={styles.buttonText}>+</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={minNumbers}>
-            <Text style={styles.buttonText}>-</Text>
+          <TouchableOpacity style={styles.button} onPress={comparing}>
+            <Text style={styles.buttonText}>Make a guess
+            </Text>
           </TouchableOpacity>
         </View>
+        <Modal
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={toggleModal}
+        >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>You guessed the number in {guess} guesses</Text>
+            <TouchableOpacity style={styles.modalButton} onPress={reset}>
+              <Text style={styles.modalButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
       </SafeAreaView>
       <StatusBar style="auto" />
     </View>
@@ -89,6 +109,33 @@ const styles = StyleSheet.create({
     marginHorizontal: 2,
   },
   buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  modalButton: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  modalButtonText: {
     color: 'white',
     fontSize: 16,
   },
